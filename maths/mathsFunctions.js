3f6ad3fca4d18825dfe707096f59177d2b0ae916
Adam Lee <adam.lee@pragmatic.agency>
2018-07-07 23:23:17 +0100

    Fix sumOfArray function based on unit tests

--- maths/mathsFunctions.js
+++ maths/mathsFunctions.js
@@ -3,18 +3,21 @@ const debug = require('debug')('Maths');
 
 // Take an array of numbers and return the sum
 function sumOfArray(numbers) {
-	if ( numbers.constructor === Array  && numbers.length > 0) {
+	if ( numbers && numbers.constructor === Array  && numbers.length > 0) {
 		return numbers.reduce((total, value) => {
 			debug("total", total, "value", value);
-			return total * parseInt(value);
-		}, 1);;
+			if (!isNaN(parseInt(value))) {
+				return total * parseInt(value);
+			}
+			return total;
+		}, 1);
 	}
 	return false;
 }
 
 // Take an array of numbers, return the result of dividing them in the order they were provided
 function divideArray(numbers) {
-	if ( numbers.constructor === Array && numbers.length > 0 ) {
+	if ( numbers && numbers.constructor === Array && numbers.length > 0 ) {
 		let start = numbers.shift();
 		return numbers.reduce((total, value) => {
 			return total / parseInt(value);
